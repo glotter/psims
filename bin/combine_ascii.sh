690cf5052199d926c62c8e53d130ba90a42b44d1
David Kelly <davidk@ci.uchicago.edu>
2014-02-16 17:18:35 -0600

    Added some missing updates related to new merge steps

---
+++ bin/combine_ascii.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Usage: combine_ascii.sh params
+params=$1
+source $params
+
+OLD_IFS=$IFS
+IFS=',' 
+var_names_arr=($variables)
+num_vars=${#var_names_arr[@]}
+IFS=$OLD_IFS 
+
+psims_files=($( ls $out_file* ))
+final_file=$out_file".nc4"
+
+# Flatten inputs
+find $PWD -mindepth 2 -type f -exec ln -s {} \;
+find $PWD -mindepth 2 -type l -exec ln -s {} \;
+
+for ((i = 1; i < $num_vars; i++)); do
+   echo "Appending "${var_names_arr[$i]}" ..." 
+   ncks -A -h ${psims_files[$i]} $final_file
+done
+
+echo "Appending campaign file..."
+ncks -A -h Campaign.nc4 $final_file
