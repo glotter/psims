41f2172a566cdcd1d3cec9942d1a14733f3dc3dc
David Kelly <davidk@ci.uchicago.edu>
2014-02-16 17:38:00 -0600

    Minor updates, remove a few debugging statements, remove '@'s in front of functions

--- RunpSIMS.swift
+++ RunpSIMS.swift
@@ -20,24 +20,24 @@ app (file outfile) combine (file inputArray[], file scenarioInput[], file param)
 
 string gridLists[] = readData("gridList.txt");
 file part_outputs[][];
-file scenario_input[] <filesys_mapper; location=@arg("campaign"), pattern="*">;
-file common_input[] <filesys_mapper; location=@arg("refdata"), pattern="*">;	
-file binary_input[] <fixed_array_mapper; files=@arg("bintransfer")>; 
+file scenario_input[] <filesys_mapper; location=arg("campaign"), pattern="*">;
+file common_input[] <filesys_mapper; location=arg("refdata"), pattern="*">;	
+file binary_input[] <fixed_array_mapper; files=arg("bintransfer")>; 
 file params <"params.psims">;
 
 foreach g,i in gridLists {
 
-   string gridNames[] = @strsplit(g, "/");
-   int lat = @toInt(gridNames[0]);
-   int lon = @toInt(gridNames[1]);
+   string gridNames[] = strsplit(g, "/");
+   int lat = toInt(gridNames[0]);
+   int lon = toInt(gridNames[1]);
 
    // Input files
-   file weather_input[] <filesys_mapper; location=@strcat(@arg("weather"), "/", gridLists[i]), pattern="*">; 
-   file soils_input[] <filesys_mapper; location=@strcat(@arg("soils"), "/", gridLists[i]), pattern="*">; 
+   file weather_input[] <filesys_mapper; location=strcat(arg("weather"), "/", gridLists[i]), pattern="*">; 
+   file soils_input[] <filesys_mapper; location=strcat(arg("soils"), "/", gridLists[i]), pattern="*">; 
 
    // Output files
-   file tar_output <single_file_mapper; file=@strcat("output/", gridLists[i], "output.tar.gz")>;
-   file part_output <single_file_mapper; file=@strcat("parts/", gridLists[i], ".psims.nc")>;
+   file tar_output <single_file_mapper; file=strcat("output/", gridLists[i], "output.tar.gz")>;
+   file part_output <single_file_mapper; file=strcat("parts/", gridLists[i], ".psims.nc")>;
 
    // Run pSIMS
    (tar_output, part_output) = RunpSIMS(scenario_input, weather_input, soils_input, common_input, binary_input, gridNames[0], gridNames[1], params);
@@ -48,21 +48,21 @@ foreach g,i in gridLists {
 # Append
 file lat_outs[];
 foreach latitude,latval in part_outputs {
-  file latout <single_file_mapper; file=@strcat("lat", latval, ".tar.gz")>;
+  file latout <single_file_mapper; file=strcat("lat", latval, ".tar.gz")>;
   latout = append(part_outputs[latval], params);
   lat_outs[latval] = latout;
 }
 
 # Merge
 string variable_array[];
-variable_array = @strsplit(@arg("variables"), ",");
+variable_array = strsplit(arg("variables"), ",");
 file var_ncs[];
 foreach v,i in variable_array {
-   file var_nc <single_file_mapper; file=@strcat(@arg("out_file"), ".", v, ".nc4")>;
+   file var_nc <single_file_mapper; file=strcat(arg("out_file"), ".", v, ".nc4")>;
    var_nc = merge(i+1, lat_outs, params);
    var_ncs[i] = var_nc;
 }
 
 # Combine 
-file nc <single_file_mapper; file=@strcat(@arg("out_file"), ".nc4")>;
+file nc <single_file_mapper; file=strcat(arg("out_file"), ".nc4")>;
 nc = combine(var_ncs, scenario_input, params);
