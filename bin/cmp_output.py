26c565d0414c5f74e706893721ac8af6eafeb4a0
David Kelly <davidkelly999@gmail.com>
2014-03-14 11:19:04 -0500

    Generate checksums and compare contents of tar files

--- bin/cmp_output.py
+++ bin/cmp_output.py
@@ -3,24 +3,70 @@
 import filecmp
 import os
 import sys
+import tarfile
+import hashlib
+
+# Given a tar file, return a list of checksums for files contained within
+def tar_checksums(tar_fn):
+    tar = tarfile.open(tar_fn)
+    sums = {} 
+    chunk = 1024*1024
+    for member in tar:
+        if not member.isfile():
+            continue
+        f = tar.extractfile(member)
+        h = hashlib.md5()
+        data = f.read(chunk)
+        while data:
+            h.update(data)
+            data = f.read(chunk)
+        sums[member.name] = h.hexdigest()
+    return sums
+
+# Compare two tar files (return True if they're identical)
+def tarcmp(tar1, tar2):
+   tar1_sums = tar_checksums(tar1)
+   tar2_sums = tar_checksums(tar2)
+   retval = True
+
+   for key in set(tar1_sums.keys()).union(tar2_sums.keys()):
+       if key not in tar1_sums:
+           print "%s doesn't contain file %s" % (tar1, key)
+           retval = False
+       elif key not in tar2_sums:
+           print "%s doesn't contain file %s" % (tar2, key)
+           retval = False
+       elif tar1_sums[key] != tar2_sums[key]:
+           print "File %s (in %s) differs" % (key, os.path.basename(tar1))
+           retval = False
+   return retval
 
 # Command line arguments
 if len(sys.argv) != 3:
     sys.exit("Usage: %s <run_directory> <test_directory>" % sys.argv[0])
 
-run_directory = os.path.normpath(sys.argv[1])
-test_directory = os.path.normpath(sys.argv[2])
+run_dir  = os.path.normpath(sys.argv[1])
+test_dir = os.path.normpath(sys.argv[2])
 
-# Open list of files to compare
-file_list_filename = os.path.join(test_directory, "files.list")
-if not os.path.isfile(file_list_filename):
-	sys.exit("Error: Unable to find file %s" % file_list_filename)
+# Verify test.txt exists
+test_fn = os.path.join(test_dir, "test.txt")
+if not os.path.isfile(test_fn):
+	sys.exit("Error: Unable to find test file %s" % test_file_fn)
+test_file = open(test_fn, "r")
 
-file_list = open(file_list_filename, "r")
+# Compare files in test.txt
+ec = 0
+for fn in iter(test_file):  
+   fn = fn.rstrip()
+   filecmp1 = os.path.abspath(os.path.join(test_dir, fn))
+   filecmp2 = os.path.abspath(os.path.join(run_dir, fn))
 
-# Compare files in list
-for file_to_compare in iter(file_list):  
-   test_file = os.path.abspath(os.path.join(test_directory, file_to_compare.rstrip()))
-   run_file = os.path.abspath(os.path.join(run_directory, file_to_compare.rstrip()))
-   if not filecmp.cmp(test_file, run_file):
-	print "File %s does not match!" % run_file
+   if fn.endswith(".tar.gz"):
+      if not tarcmp(filecmp1, filecmp2):
+         ec=1
+   else:
+       if not filecmp.cmp(filecmp1, filecmp2):
+           ec=1
+           print "File %s does not match!" % filecmp2
+
+sys.exit(ec)
